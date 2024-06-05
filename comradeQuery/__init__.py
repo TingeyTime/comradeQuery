fde87bdc51a1d8d0ceab2ee1dd79ab19e60f2fa2
Nate Tingey <ntingey@gmail.com>
2024-06-05 10:38:14 -0600

    add verbose to test

--- comradeQuery/__init__.py
+++ comradeQuery/__init__.py
@@ -2,14 +2,14 @@ from .sql_reader import SQLReader
 from .FSM import Query_Parse_FSM
 # from .scanner import Scanner
 
-def process_sql_file(file_path: str) -> list:
+def process_sql_file(file_path: str, verbose = False) -> list:
     reader = SQLReader(file_path=file_path)
     sql_text = reader.read_file()
 
     # scanner = Scanner()
     # queries = scanner.process(sql_text)
 
-    fsm = Query_Parse_FSM(sql_text)
+    fsm = Query_Parse_FSM(sql_text, verbose=verbose)
     fsm.run()
     
     return fsm.queries
--- test.py
+++ test.py
@@ -6,6 +6,6 @@ FILE_NAME = 'test.sql'
 START_TIME = time.time()
 
 print(f"Processing SQL file: {FILE_NAME}")
-q = process_sql_file(FILE_NAME)
+q = process_sql_file(FILE_NAME, verbose=True)
 print(f"Processing SQL file took {time.time() - START_TIME: .4} seconds.")
 print(q)
